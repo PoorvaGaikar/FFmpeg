07d3c5f29f2b0530e8bdc523eb1aec700cbe87b1
PoorvaGaikar <2003gaikarpoorva.com>
2024-07-02 23:49:38 +0530

    SI

--- libavfilter/vf_siti.c
+++ libavfilter/vf_siti.c
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2021 Boris Baracaldo
  * Copyright (c) 2022 Thilo Borgmann
@@ -68,10 +67,12 @@ typedef struct SiTiContext {
     float max_ti;
     float min_si;
     float min_ti;
+    float si;
     float sum_si;
     float sum_ti;
     float *gradient_matrix;
     float *motion_matrix;
+    float *oetf_data;
     int full_range;
     int print_summary;
 } SiTiContext;
@@ -112,11 +113,12 @@ static av_cold void uninit(AVFilterContext *ctx)
     av_freep(&s->prev_frame);
     av_freep(&s->gradient_matrix);
     av_freep(&s->motion_matrix);
+    av_freep(&s->oetf_data);
 }
 
 static int config_input(AVFilterLink *inlink)
 {
-    // Video input data avilable
+    // Video input data available
     AVFilterContext *ctx = inlink->dst;
     SiTiContext *s = ctx->priv;
     int max_pixsteps[4];
@@ -124,14 +126,16 @@ static int config_input(AVFilterLink *inlink)
     size_t data_sz;
     size_t gradient_sz;
     size_t motion_sz;
+    size_t oetf_sz;
 
     const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(inlink->format);
     av_image_fill_max_pixsteps(max_pixsteps, NULL, desc);
 
-    // free previous buffers in case they are allocated already
+    // Free previous buffers in case they are allocated already
     av_freep(&s->prev_frame);
     av_freep(&s->gradient_matrix);
     av_freep(&s->motion_matrix);
+    av_freep(&s->oetf_data);
 
     s->pixel_depth = max_pixsteps[0];
     s->width = inlink->w;
@@ -147,7 +151,10 @@ static int config_input(AVFilterLink *inlink)
     motion_sz = s->width * sizeof(float) * s->height;
     s->motion_matrix = av_malloc(motion_sz);
 
-    if (!s->prev_frame || !s->gradient_matrix || !s->motion_matrix) {
+    oetf_sz = s->width * sizeof(float) * s->height;
+    s->oetf_data = av_malloc(oetf_sz);
+
+    if (!s->prev_frame || !s->gradient_matrix || !s->motion_matrix || !s->oetf_data) {
         return AVERROR(ENOMEM);
     }
 
@@ -179,26 +186,22 @@ static uint16_t convert_full_range(int factor, uint16_t y)
     return (full_upper * limit_y / limit_upper);
 }
 
+static float normalize_to_original_si_range(float si_value) {
+    return si_value * 255.0;
+}
+
 // Applies sobel convolution
-static void convolve_sobel(SiTiContext *s, const uint8_t *src, float *dst, int linesize)
-{
-    double x_conv_sum;
-    double y_conv_sum;
+static void convolve_sobel(SiTiContext *s, float *src, float *dst, int linesize) {
+    double x_conv_sum, y_conv_sum;
     float gradient;
-    int ki;
-    int kj;
-    int index;
-    uint16_t data;
+    int ki, kj, index;
     int filter_width = 3;
     int filter_size = filter_width * filter_width;
     int stride = linesize / s->pixel_depth;
-    // For 8 bits, limited range goes from 16 to 235, for 10 bits the range is multiplied by 4
-    int factor = s->pixel_depth == 1 ? 1 : 4;
+    double gradient_sum = 0.0;
 
-    // Dst matrix is smaller than src since we ignore edges that can't be convolved
-    #define CONVOLVE(bps)                                           \
+    #define CONVOLVE()                                              \
     {                                                               \
-        uint##bps##_t *vsrc = (uint##bps##_t*)src;                  \
         for (int j = 1; j < s->height - 1; j++) {                   \
             for (int i = 1; i < s->width - 1; i++) {                \
                 x_conv_sum = 0.0;                                   \
@@ -207,22 +210,30 @@ static void convolve_sobel(SiTiContext *s, const uint8_t *src, float *dst, int l
                     ki = k % filter_width - 1;                      \
                     kj = floor(k / filter_width) - 1;               \
                     index = (j + kj) * stride + (i + ki);           \
-                    data = s->full_range ? vsrc[index] : convert_full_range(factor, vsrc[index]); \
-                    x_conv_sum += data * X_FILTER[k];               \
-                    y_conv_sum += data * Y_FILTER[k];               \
+                    x_conv_sum += src[index] * X_FILTER[k];         \
+                    y_conv_sum += src[index] * Y_FILTER[k];         \
                 }                                                   \
                 gradient = sqrt(x_conv_sum * x_conv_sum + y_conv_sum * y_conv_sum); \
-                dst[(j - 1) * (s->width - 2) + (i - 1)] = gradient; \
+                dst[(j - 1) * (s->width - 2) + (i - 1)] = gradient; \                                        
             }                                                       \
         }                                                           \
     }
 
-    if (s->pixel_depth == 2) {
-        CONVOLVE(16);
-    } else {
-        CONVOLVE(8);
+    CONVOLVE();
+
+    // Calculate the standard deviation of the gradient magnitudes (SI)
+    float mean_value = gradient_sum / ((s->width - 2) * (s->height - 2));
+    float sum_squared_diff = 0.0;
+    for (int j = 0; j < s->height - 2; j++) {
+        for (int i = 0; i < s->width - 2; i++) {
+            float diff = dst[j * (s->width - 2) + i] - mean_value;
+            sum_squared_diff += diff * diff;
+        }
     }
+    float si = sqrt(sum_squared_diff / ((s->width - 2) * (s->height - 2)));
+
 }
+
 
 // Calculate pixel difference between current and previous frame, and update previous
 static void calculate_motion(SiTiContext *s, const uint8_t *curr, float *motion_matrix, int linesize)
@@ -281,19 +292,6 @@ static float std_deviation(float *img_metrics, int width, int height)
     }
     sqr_diff = sqr_diff / size;
     return sqrt(sqr_diff);
-}
-
-static float mean(float *img_metrics, int width, int height)
-{
-    int size = height * width;
-    double sum = 0.0;
-
-    for (int j = 0; j < height; j++){
-        for (int i = 0; i < width; i++){
-            sum += img_metrics[j * width + i];
-        }
-    }
-    return sum / size;
 }
 
 static void set_meta(AVDictionary **metadata, const char *key, float d)
@@ -378,15 +376,11 @@ static float apply_pq_oetf(float signal) {
 }
 
 // Normalize luma values, apply EOTF, scale luminance, and apply OETF
-static void normalize_apply_eotf_scale_oetf(uint8_t *data, int width, int height, int linesize, int bit_depth, int full_range, int domain) {
+static void normalize_apply_eotf_scale_oetf(uint8_t *data, float *oetf_data, int width, int height, int linesize, int bit_depth, int full_range, int domain) {
     int max_val = (1 << bit_depth) - 1;
     int stride;
     float L_min = 0.1;  // Example minimum luminance of the display
     float L_max = 300.0;  // Example maximum luminance of the display
-
-    // Print any one pixel's normalized and scaled value from each frame
-    int print_x = width / 2;
-    int print_y = height / 2;
 
     if (bit_depth == 8) {
         stride = linesize / sizeof(uint8_t);
@@ -400,12 +394,7 @@ static void normalize_apply_eotf_scale_oetf(uint8_t *data, int width, int height
                 // Scale luminance to the display range
                 float scaled_value = L_min + (linear_light_value * (L_max - L_min));
                 float encoded_value = apply_pq_oetf(scaled_value);
-                if (j == print_y && i == print_x) {
-                    av_log(NULL, AV_LOG_INFO, "Normalized value (8-bit): %f\n", normalized_value);
-                    av_log(NULL, AV_LOG_INFO, "Linear light value (8-bit): %f\n", linear_light_value);
-                    av_log(NULL, AV_LOG_INFO, "Scaled value (8-bit): %f\n", scaled_value);
-                    av_log(NULL, AV_LOG_INFO, "Encoded value (8-bit): %f\n", encoded_value);
-                }
+                oetf_data[j * stride + i] = encoded_value; 
                 data[j * stride + i] = encoded_value * max_val; // scale back to original range
             }
         }
@@ -422,12 +411,7 @@ static void normalize_apply_eotf_scale_oetf(uint8_t *data, int width, int height
                 // Scale luminance to the display range
                 float scaled_value = L_min + (linear_light_value * (L_max - L_min));
                 float encoded_value = apply_pq_oetf(scaled_value);
-                if (j == print_y && i == print_x) {
-                    av_log(NULL, AV_LOG_INFO, "Normalized value (10-bit): %f\n", normalized_value);
-                    av_log(NULL, AV_LOG_INFO, "Linear light value (10-bit): %f\n", linear_light_value);
-                    av_log(NULL, AV_LOG_INFO, "Scaled value (10-bit): %f\n", scaled_value);
-                    av_log(NULL, AV_LOG_INFO, "Encoded value (10-bit): %f\n", encoded_value);
-                }
+                oetf_data[j * stride + i] = encoded_value; 
                 data16[j * stride + i] = encoded_value * max_val; // scale back to original range
             }
         }
@@ -446,14 +430,18 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame) {
 
     // Normalize luma values, apply EOTF, scale luminance, and apply OETF
     int domain = 0; // Change this as needed (0 for SDR, 1 for HLG, 2 for PQ)
-    normalize_apply_eotf_scale_oetf(frame->data[0], s->width, s->height, frame->linesize[0], s->pixel_depth * 8, s->full_range, domain);
+    normalize_apply_eotf_scale_oetf(frame->data[0], s->oetf_data, s->width, s->height, frame->linesize[0], s->pixel_depth * 8, s->full_range, domain);
 
     // Calculate SI and TI
-    convolve_sobel(s, frame->data[0], s->gradient_matrix, frame->linesize[0]);
+    convolve_sobel(s, s->oetf_data, s->gradient_matrix, frame->linesize[0]);
     calculate_motion(s, frame->data[0], s->motion_matrix, frame->linesize[0]);
     si = std_deviation(s->gradient_matrix, s->width - 2, s->height - 2);
     ti = std_deviation(s->motion_matrix, s->width, s->height);
-
+    
+    // Print the denormalized SI value
+    float normalized_si = normalize_to_original_si_range(si);
+    av_log(NULL, AV_LOG_INFO, "SI %f\n", normalized_si);
+    
     // Calculate statistics
     s->max_si  = fmaxf(si, s->max_si);
     s->max_ti  = fmaxf(ti, s->max_ti);
@@ -468,6 +456,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame) {
 
     return ff_filter_frame(inlink->dst->outputs[0], frame);
 }
+
 
 #define OFFSET(x) offsetof(SiTiContext, x)
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
